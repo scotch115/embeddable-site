2c183ce880691547414a9761b4567ae93112878f
scotch115 <jordangamache115@gmail.com>
2022-02-22 21:57:25 -0500

    Developed a website to allow users to provide a google sheet or doc embed and change the width and height of the embed visually.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Slider } from 'primereact/slider';
+
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.css';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState('');
+  const [sliderWidth, setSliderWidth] = useState(500);
+  const [sliderHeight, setSliderHeight] = useState(500);
+  const [header, setHeader] = useState('Enter a Google Site to generate an embed link')
+  const [validURL, setValidURL] = useState(false);
+
+  const filterURL = (url) => {
+    var filterText = url.match(/(?:"[^"]*"|^[^"]*$)/)[0].replace(/"/g, "");
+    setValue(filterText);
+  }
+
+  useEffect(() => {
+    if (value && value.includes('docs.google.com')) {
+      setHeader('Enter a Google Site to generate an embed link');
+      filterURL(value);
+      setValidURL(true);
+      console.log('google doc');
+    } else if (value && value.includes('sheets.google.com')) {
+      setHeader('Enter a website here to generate an embed link');
+      filterURL(value);
+      setValidURL(true);
+      console.log('google sheets');
+    } else if (value !== '' && value.includes('.com') && !value.includes('docs.google.com' || 'sheets.google.com')) {
+      setHeader('Please enter a valid google forms or google sheets url');
+      setValidURL(false);
+    } else {
+      setHeader('Enter a Google Site to generate an embed link');
+      setValidURL(false)
+    }
+  }, [value, setValue, header, setHeader, validURL, setValidURL]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{header}</h1>
+      <br />
+      <span className='p-float-label p-input-icon-left'>
+        <i className='pi pi-link' />
+        <InputText
+          id="link"
+          name="link"
+          value={value} 
+          onChange={(e) => {setValue(e.target.value)}} />
+          <label htmlFor='link'> Embed Link </label>
+      </span>
+      <div style={{ paddingTop: '20px' }}>
+        {(validURL) 
+          ? <div>
+              <iframe src={value} width={sliderWidth} height={sliderHeight}/>
+              <h5> Width: {sliderWidth} </h5>
+              <Slider
+                value={sliderWidth}
+                onChange={(e) => {setSliderWidth(e.value)}}
+                min={100} 
+                max={1000}
+                step={10} 
+              />
+              <h5> Height: {sliderHeight} </h5>
+              <Slider
+                value={sliderHeight}
+                onChange={(e) => {setSliderHeight(e.value)}}
+                min={100}
+                max={1000}
+                step={10} 
+              />
+              <br />
+              <span>
+                <h5> Generated Embed Link: </h5>
+                <p className='generatedLink'>
+                  {`<iframe src="${value}" width="${sliderWidth}" height="${sliderHeight}" />`}
+                </p>
+              </span>
+            </div>
+          : null}
+      </div>
     </div>
   );
 }
